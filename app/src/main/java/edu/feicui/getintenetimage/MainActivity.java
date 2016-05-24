8f441cfc42693d0adf929b1f05b6cccce3dc6859
Administrator <wonner_cw@163.com>
2016-05-24 11:23:03 +0800

    新添加一个输入框，可以手动输入地址，以获取图片，如果地址不正确，则弹出本地图片，提示地址不正确

--- app/src/main/java/edu/feicui/getintenetimage/MainActivity.java
+++ app/src/main/java/edu/feicui/getintenetimage/MainActivity.java
@@ -66,51 +66,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-//
-//    private static final String TAG      = "ma";
-//    private static final int    LOAD_PIC = 1;
-//    private Button    mButton;
-//    private ImageView mImageView;
-//    private Bitmap    mBitmap;
-//    public final String urlPathContent = "http://c.cotton.netease.com/buckets/1BquJi/files/02FrwTHV3h.jpeg";
-//
-//    private Handler handler = new Handler() {
-//        @Override
-//        public void handleMessage(Message msg) {
-//            switch (msg.what) {
-//                case LOAD_PIC:
-//                    mImageView.setImageBitmap(mBitmap);
-//                    Toast.makeText(getApplicationContext(), "显示成功", Toast.LENGTH_SHORT).show();
-//                    break;
-//            }
-//        }
-//    };
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        mButton = (Button) findViewById(R.id.btn_getimage);
-//        mImageView = (ImageView) findViewById(R.id.iv_image);
-//
-//        mButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new Thread() {
-//                    @Override
-//                    public void run() {
-//                        try {
-//                            byte[] data = ImageService.getImages(urlPathContent);
-//                            mBitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-//                            handler.sendEmptyMessage(LOAD_PIC);
-//                        } catch (IOException e) {
-//                            Toast.makeText(getApplicationContext(), "连接超时", Toast.LENGTH_SHORT).show();
-//                            e.printStackTrace();
-//                        }
-//
-//                    }
-//                }.start();
-//            }
-//        });
-//    }
+
 }
